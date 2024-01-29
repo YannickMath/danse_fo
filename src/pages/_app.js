2e8e247d0485489610109948a7de094268b3e0c2
Yannick MATH <yannick_math@yahoo.fr>
2024-01-29 21:51:16 +0100

    reducer and persistStore installation , initialization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,34 @@
-import Layout from '../components/layout'
-import '@/styles/globals.css'
-import 'animate.css';
+import Layout from "../components/layout";
+import "@/styles/globals.css";
+import "animate.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import counter from "../components/reducer/counterSlice";
 
 export default function App({ Component, pageProps }) {
+  const reducers = combineReducers({ counter });
+
+  const persistConfig = { key: "fast_food", storage };
+
+  const store = configureStore({
+    reducer: persistReducer(persistConfig, reducers),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({ serializableCheck: false }),
+  });
+
+  const persistor = persistStore(store);
+
   return (
-    <Layout>
-  <Component {...pageProps} />
-    </Layout>
-  )
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
+    </Provider>
+  );
 }
